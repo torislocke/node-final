94f666b1718fa4fb72b7742405563a031924ee5c
Tori Locke <tori.locke@hotmail.com>
2021-09-18 08:36:34 -0700

    Add static middleware to serve css file

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 // third party installs
 const express = require('express'); // import express framework for utilities
 const bodyParser = require('body-parser');
@@ -8,14 +7,16 @@ const app = express(); // run express as a function as pass to createServer as h
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { dirname } = require('path');
+
 app.use(bodyParser.urlencoded({extended: false})); // middleware function to parse body
+app.use(express.static(path.join(__dirname, 'public')));  // read access to public folder
+
 
 app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join__dirname, 'views', 'pageNotFound.html');
+    res.status(404).sendFile(path.join(__dirname, 'views', 'pageNotFound.html'));
 });
 
 app.listen(5000) // app object uses express listen to create server
