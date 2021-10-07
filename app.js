a39a1521c8a41b9ccd3fa0accd948db7614b681a
Tori Locke <tori.locke@hotmail.com>
2021-10-06 19:36:33 -0700

    correct port for Heroku

--- app.js
+++ app.js
@@ -1,76 +1,79 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const MONGODB_URI =
-    "mongodb+srv://tlocke:MongoDB12@cluster0.sd8ux.mongodb.net/ecommerce?retryWrites=true&w=majority";
-    const app = express();
-    const store = new MongoDBStore({
-      uri: MONGODB_URI,
-      collection: 'sessions'
-    });
-    
-    app.set('view engine', 'ejs');
-    app.set('views', 'views');
-    
-    const adminRoutes = require('./routes/admin');
-    const shopRoutes = require('./routes/shop');
-    const authRoutes = require('./routes/auth');
-    
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(
-      session({
-        secret: 'my secret',
-        resave: false,
-        saveUninitialized: false,
-        store: store
-      })
-    );
-    
-    app.use((req, res, next) => {
-      if (!req.session.user) {
-        return next();
-      }
-      User.findById(req.session.user._id)
-        .then(user => {
-          req.user = user;
-          next();
-        })
-        .catch(err => console.log(err));
-    });
-    
-    app.use('/admin', adminRoutes);
-    app.use(shopRoutes);
-    app.use(authRoutes);
-    
-    app.use(errorController.get404);
-    
-    mongoose
-      .connect(MONGODB_URI)
-      .then(result => {
-        User.findOne().then(user => {
-          if (!user) {
-            const user = new User({
-              name: 'Tori',
-              email: 'tbos@mail.com',
-              cart: {
-                items: []
-              }
-            });
-            user.save();
-          }
+  "mongodb+srv://tlocke:MongoDB12@cluster0.sd8ux.mongodb.net/ecommerce?retryWrites=true&w=majority";
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Tori",
+          email: "tbos@mail.com",
+          cart: {
+            items: [],
+          },
         });
-        app.listen(5000);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    
+        user.save();
+      }
+    });
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+      port = 5000;
+    }
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
