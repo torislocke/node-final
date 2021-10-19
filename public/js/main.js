d5ca5ca12c4af7ee8c1c05e0557a395ffb978fd7
Tori Locke <tori.locke@hotmail.com>
2021-10-19 14:17:23 -0700

    more styling changes - still need to finish

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,15 @@ function myFunction() {
   }
 }
 
+var navList = document.getElementById("nav-lists");
+function Show() {
+navList.classList.add("_Menus-show");
+}
+
+function Hide(){
+navList.classList.remove("_Menus-show");
+}
+
 
 mongoose
   .connect(
